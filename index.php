ed810d9a22e4a516f81c4547aa93a61f2dea579b
vohuunhan5 <45483301+vohuunhan5@users.noreply.github.com>
2018-11-30 16:19:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,132 @@
+
+<html>
+<head>
+<title>LoLi Team</title>
+<meta charset="utf-8" />
+<meta name="Author" content="Shiina EoPi">
+<meta name="copyright" content="LoLi Sec Team">
+<meta name="description" content="Welcome to website of LoLi Sec Team">
+<meta property="og:image" content="https://loliteam.net/title.png">
+<meta property="og:title" content="LOLI SEC TEAM">
+<link rel="shortcut icon" href="favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+<link rel="stylesheet" href="assets/css/main.css" />
+<noscript>
+        <link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+<script language=javascript>
+        title_tmp1 = document.title
+        if (title_tmp1.indexOf(">>") != -1) {
+            title_tmp2 = title_tmp1.split(">>");
+            title_last = "*~*" + title_tmp2[1];
+            title_last = title_last + "*~*" + title_tmp2[2];
+        } else {
+
+            if (title_tmp1.indexOf("*~*") != -1) {
+                title_tmp2 = title_tmp1.split("*~*");
+                title_last = "*~*" + title_tmp2[1];
+                if (title_last == "*~*") {
+                    title_last = "*~*"
+                };
+                if (title_last == "*~*") {
+                    title_last = "*~*"
+                };
+            } else {
+                title_last = "LoLi Team"
+            }
+        }
+
+
+        title_new = "" + title_last + ""
+        step = 0
+
+        function flash_title() {
+            step++
+            if (step == 8) {
+                step = 1
+            }
+            if (step == 1) {
+                document.title = '|----♥' + title_new + '♥----|'
+            }
+            if (step == 2) {
+                document.title = '|---♥-' + title_new + '-♥---|'
+            }
+            if (step == 3) {
+                document.title = '|--♥--' + title_new + '--♥--|'
+            }
+            if (step == 4) {
+                document.title = '|-♥---' + title_new + '---♥-|'
+            }
+            if (step == 5) {
+                document.title = '|♥----' + title_new + '----♥|'
+            }
+            if (step == 6) {
+                document.title = '|-♥---' + title_new + '---♥-|'
+            }
+            if (step == 7) {
+                document.title = '|--♥--' + title_new + '--♥--|'
+            }
+            if (step == 8) {
+                document.title = '|---♥-' + title_new + '-♥---|'
+            }
+            if (step == 9) {
+                document.title = '|----♥' + title_new + '♥----|'
+            }
+            setTimeout("flash_title()", 180);
+        }
+        flash_title()
+    </script>
+</head>
+<body class="is-preload">
+<div id="wrapper">
+<section id="main">
+<header>
+<span class="avatar"><img src="images/avatar.jpg" alt="" /></span>
+<h1>
+<font size="9" style="background: url(&quot;src/chopnhay.gif&quot;) 
+						repeat scroll 0% 0% transparent; color:white;text-shadow: 0 0 0.2em #241D1B,0 0 0.2em #241D1B">LoLi Team</font>
+</h1>
+<p>No Definition Of</br> | " Can Not " |</br>| " Safety System " |</br>| " Key And CopyRight " |</p>
+</header>
+<footer>
+<ul class="icons">
+<li><a href="http://blog.hellcatvn.com" target="_blank" class="fa-rss">Blog</a></li>
+<li><a href="https://198.13.43.237:4444/" target="_blank" class="fa-shield">Firewall</a></li>
+<li><a href="https://loli-s.info/" target="_blank" class="fa-heartbeat">LoLi S</a></li>
+<li><a href="https://www.youtube.com/channel/UC0HE9IQZ6X85f1dequ07bxA" target="_blank" class="fa-youtube">Youtube</a></li>
+<li><a href="https://loliteam.net/adidaphat/" target="_blank" class="fa-eye">Phat</a></li>
+<li><a href="https://ctf.loliteam.net/" target="_blank" class="fa-fire">CTF</a></li>
+<li><a href="https://chat.loliteam.net/" target="_blank" class="fa-comments">Chat Server</a></li>
+ </ul>
+</footer>
+<span class="avatar"><img src="images/tim.jpg" alt="" /></span>
+<ul class="actions special">
+<li><a href="Time" class="button" target="_blank">LoLi Time</a></li>
+<li><a href="SystemMusic" class="button" target="_blank">SystemMusic</a></li>
+</ul>
+<ul class="actions special">
+<li><a href="404Page" class="button" target="_blank">404Page</a></li>
+<li><a href="404File" class="button" target="_blank">404File</a></li>
+</ul>
+<ul class="actions special">
+<li><a href="Info" class="button" target="_blank">News</a></li>
+<li><a href="Crack" class="button" target="_blank">Hack</a></li>
+</ul>
+</section>
+<footer id="footer">
+<ul class="copyright">
+<li>&copy; LoLi Team</li>
+<li>Power by: <a href="https://www.facebook.com/Thuong.EoPi" target="_blank">EoPi</a></li>
+</ul>
+</footer>
+</div>
+<script>
+        if ('addEventListener' in window) {
+            window.addEventListener('load', function() {
+                document.body.className = document.body.className.replace(/\bis-preload\b/, '');
+            });
+            document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+        }
+    </script>
+<iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/427445127&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+</body>
+</html>
